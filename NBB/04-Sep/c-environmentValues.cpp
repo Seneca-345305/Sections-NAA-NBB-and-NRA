e5a427b15e7d2b7c8712cd559215cd51ed8aa3d2
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-09-15 11:36:21 -0400

    nbb sep15

---
+++ NBB/04-Sep/c-environmentValues.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[], char* env[]) {
+   cout << "04Sep15 OOP345NAA" << endl;
+   cout << argc << " command argurmets: " << endl;
+   for (int i = 0; i < argc; i++) {
+      cout << (i + 1) << ": " << argv[i] << endl;
+   }
+   cout << endl << "Environment Variables:" << endl;
+   for (int i = 0; env[i] ; i++) {
+      cout << (i + 1) << ": " << env[i] << endl;
+   }
+
+   return 0;
+}
