d20cb0abccf1c39b53ed555866e6cd4d197edfb8
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2025-09-16 15:15:35 -0400

    345NRA Sep16

---
+++ NRA/03-Sep16/b-environmentVairables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char* argv[], char* env[]) {
+   cout << "03-sep16 OOP345 NRA" << endl;
+   cout << endl << "Command line argurmets: " << endl;
+   for (int i = 0; i < argc; i++) {
+      cout << (i + 1) << ": " << argv[i] << endl;
+   }
+   cout << endl << "OS Environment variables: " << endl;
+   for (int i = 0; env[i] ; i++) {
+      cout << (i + 1) << ": " << env[i] << endl;
+   }
+
+   return 0;
+}
