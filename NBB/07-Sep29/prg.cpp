41f4cf9809f4f7b4c88b2d8162bde157eecd9849
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2025-09-28 20:58:04 -0400

    Week 5

---
+++ NBB/07-Sep29/prg.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "07-Sep29 OOP345 NBB" << endl;
+   return 0;
+}
---
+++ NRA/06-Sep30/prg.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "06-Sep30 OOP345 NBB" << endl;
+   return 0;
+}
