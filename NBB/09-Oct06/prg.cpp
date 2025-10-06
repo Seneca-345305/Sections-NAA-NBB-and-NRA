921ec84a28d6f86dd7563b6043c78b9c7c5315fb
Fardad Soleimanloo <fardad.soleimanloo@senecapolytechnic.ca>
2025-10-06 00:08:25 -0400

    Oct06 init

---
+++ NBB/09-Oct06/prg.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Oct06 - OOP345 NBB" << endl;
+   return 0;
+}
---
+++ NRA/08-Oct06/prg.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+   cout << "Oct06 - OOP345 NAA" << endl;
+   return 0;
+}
